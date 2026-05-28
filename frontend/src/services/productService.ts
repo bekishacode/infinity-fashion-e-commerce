4bccd2b624f875837739d3faeb4ce345aaf53e52
Bereket Fikre <bekifikre19@gmail.com>
2026-05-28 16:48:48 +0300

    Update environment variable usage

--- frontend/src/services/productService.ts
+++ frontend/src/services/productService.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://infinity-fashion-e-commerce.onrender.com';//http://localhost:8000/api/v1
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api/v1';//http://localhost:8000/api/v1
 //https://infinity-fashion-e-commerce.onrender.com
 export interface Product {
   id: number;
